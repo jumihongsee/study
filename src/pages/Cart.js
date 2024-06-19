ea8d04020218a509e2e13d3f01b18f861a3a9c71
jumihongsee <juju_32@naver.com>
2024-06-19 18:45:33 +0900

    useMemo와 memo사용법

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,8 +2,35 @@ import { Table } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 import { changeName, ageUp } from "./../store/userSlice.js"
 import { countUp, deleteItem } from "../store.js"
+import { useState, memo, useMemo } from "react"
+
+
+
+let Child = memo( function(){
+    // 상위 컴포넌트가 재렌더링 될때 자식 컴포넌트도 전부 재렌더링이 된다. 이것을 막기 위해서 꼭 필요할 때만 재렌더링 하게 만들 수 있음.
+    // 따라서 memo를 사용 
+
+    console.log('재랜더링')
+    return <div>자식임</div>
+    
+    //memo 의 원리 : props가 변할 때만 재렌더링해줌 + 보통 무거운 컴포넌트에만 붙여논다 
+
+
+}
+
+)
+
+
+function 함수(){
+    return //반복문10억번 돌린결과
+}
 
 function Cart(){
+
+    let result = 함수;
+    useMemo(()=>{ // 컴포넌트 렌더링시 1회만 실행해줌 
+        return 함수()
+    }, [])
 
     let state = useSelector((state)=>{return state})
 
@@ -16,6 +43,8 @@ function Cart(){
 
     return(
        <div>
+            <Child></Child>
+
             {state.user.name}의 장바구니 {state.user.age} <br/>
              <button
                 onClick={()=>{
